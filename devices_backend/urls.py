a848f72a94960022274d328a2e7a4a226d3e1f07
ajinkyacswg <138433791+ajinkyacswg@users.noreply.github.com>
2024-01-28 18:57:09 +0530

    added devices and locations endpoints

--- devices_backend/urls.py
+++ devices_backend/urls.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from devices.api import app
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', app.urls)
+]
+
 """
 URL configuration for devices_backend project.
 
@@ -14,9 +23,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
